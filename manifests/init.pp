71dc0e4e4f69803e968f06308cf399b9ab9f4224
Sam Morrison <sorrison@gmail.com>
2012-10-03 05:21:21 +0000

    Added ubuntu cloud archive repo

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,4 +8,14 @@ class openstack {
     ensure => present,
   }
 
+  package {'ubuntu-cloud-keyring':
+    ensure => installed,
+  }
+
+  file {ubuntu-cloud-archive-repo:
+    path    => '/etc/apt/sources.list.d/ubuntu-cloud-archive.list',
+    source  => 'puppet:///modules/openstack/ubuntu-cloud-archive.list',
+    notify  => Exec[apt-get-update],
+    require => Package['ubuntu-cloud-keyring'],
+  }
 }
