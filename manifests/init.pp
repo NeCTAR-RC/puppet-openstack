d8d3fb0e59433641afaaf1ea2f7d05109d3ad1ed
Sam Morrison <sorrison@gmail.com>
2012-11-30 09:24:22 +1100

    Only install cloud archive on precise

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,14 +12,16 @@ class openstack {
     ensure => installed,
   }
 
-  package {'ubuntu-cloud-keyring':
-    ensure => installed,
-  }
+  if $lsbdistcodename == 'precise' {
+    package {'ubuntu-cloud-keyring':
+      ensure => installed,
+    }
 
-  file {ubuntu-cloud-archive-repo:
-    path    => '/etc/apt/sources.list.d/ubuntu-cloud-archive.list',
-    source  => 'puppet:///modules/openstack/ubuntu-cloud-archive.list',
-    notify  => Exec[apt-get-update],
-    require => Package['ubuntu-cloud-keyring'],
+    file {ubuntu-cloud-archive-repo:
+      path    => '/etc/apt/sources.list.d/ubuntu-cloud-archive.list',
+      source  => 'puppet:///modules/openstack/ubuntu-cloud-archive.list',
+      notify  => Exec[apt-get-update],
+      require => Package['ubuntu-cloud-keyring'],
+    }
   }
 }
