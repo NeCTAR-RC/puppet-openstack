8d8ab9ef9d2bea16a7c1e9c6da4f5356cb168b75
Marcus Furlong <furlongm@gmail.com>
2013-02-21 17:16:41 +1100

    temp fix allows puppet to continue
    Change-Id: I31ac2719e7f6e457cdf541f9189e9aea53cd98de

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,14 +13,16 @@ class openstack {
   }
 
   if $lsbdistcodename == 'precise' {
+
     package {'ubuntu-cloud-keyring':
       ensure => installed,
+      require => Exec[apt-get-update],
     }
 
     file {ubuntu-cloud-archive-repo:
       path    => '/etc/apt/sources.list.d/ubuntu-cloud-archive.list',
       source  => 'puppet:///modules/openstack/ubuntu-cloud-archive.list',
-      notify  => Exec[apt-get-update],
+      #notify  => Exec[apt-get-update],
       require => Package['ubuntu-cloud-keyring'],
     }
   }
