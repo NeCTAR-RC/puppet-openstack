9f970d352dfd173a42f241fb538f3d5e93815e28
Sam Morrison <sorrison@gmail.com>
2013-03-21 10:10:20 +1100

    Install apt-key for cloud archive
    Change-Id: I8f443536e440ec7cdb06b7c9b893118db3205c18

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,6 +12,19 @@ class openstack {
     ensure => installed,
   }
 
+  if $::rfc1918_gateway == 'true' {
+    exec { 'cloudarchive-apt-key':
+      path    => '/usr/bin:/bin:/usr/sbin:/sbin',
+      command => "apt-key adv --keyserver pgp.mit.edu --keyserver-options http-proxy=\"${::http_proxy}\" --recv-keys EC4926EA",
+      unless  => 'apt-key list | grep 4BD6EC30 >/dev/null 2>&1',
+    }
+  } else {
+    apt::key { 'cloud-archive':
+      key         => 'EC4926EA',
+      key_server => 'pgp.mit.edu',
+    }
+  }
+
   if $lsbdistcodename == 'precise' {
 
     apt::source { 'ubuntu-cloud-archive':
