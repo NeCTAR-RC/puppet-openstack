9bd80a1b6694e3f046519c461b8c8af9cbfc1610
Sam Morrison <sorrison@gmail.com>
2013-03-22 10:28:52 +1100

    Separate out packages into own class
    Change-Id: I9c0fb024496414e9de70b6b57e7f2158ca8c9de7

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,27 +11,4 @@ class openstack {
   @package {'python-memcache':
     ensure => installed,
   }
-
-  if $::rfc1918_gateway == 'true' {
-    exec { 'cloudarchive-apt-key':
-      path    => '/usr/bin:/bin:/usr/sbin:/sbin',
-      command => "apt-key adv --keyserver pgp.mit.edu --keyserver-options http-proxy=\"${::http_proxy}\" --recv-keys EC4926EA",
-      unless  => 'apt-key list | grep 4BD6EC30 >/dev/null 2>&1',
-    }
-  } else {
-    apt::key { 'cloud-archive':
-      key         => 'EC4926EA',
-      key_server => 'pgp.mit.edu',
-    }
-  }
-
-  if $lsbdistcodename == 'precise' {
-
-    apt::source { 'ubuntu-cloud-archive':
-      location          => 'http://mirrors.melbourne.nectar.org.au/ubuntu-cloud/ubuntu',
-      release           => 'precise-updates/folsom',
-      repos             => 'main',
-      include_src       => false,
-    }
-  }
 }
