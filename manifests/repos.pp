2a8639e6573a55f6d274bb50c525ce24098c2f83
Sam Morrison <sorrison@gmail.com>
2014-04-03 10:10:27 +1100

    Only install the cloud archive on precise
    Change-Id: Ie7dd75716345e0cb916aeeb97bef2af77bfc68b5

--- manifests/repos.pp
+++ manifests/repos.pp
@@ -2,15 +2,18 @@ class openstack::repos {
 
   $openstack_version = hiera('openstack_version')
 
-  apt::key { 'cloud-archive':
-    key         => 'EC4926EA',
-    key_server => 'pgp.mit.edu',
-  }
+  if $lsbdistcodename == 'precise' {
 
-  apt::source { 'ubuntu-cloud-archive':
-    location          => 'http://mirrors.melbourne.nectar.org.au/ubuntu-cloud/ubuntu',
-    release           => "${lsbdistcodename}-updates/${openstack_version}",
-    repos             => 'main',
-    include_src       => false,
+    apt::key { 'cloud-archive':
+      key         => 'EC4926EA',
+      key_server => 'pgp.mit.edu',
+    }
+
+    apt::source { 'ubuntu-cloud-archive':
+      location          => 'http://mirrors.melbourne.nectar.org.au/ubuntu-cloud/ubuntu',
+      release           => "${lsbdistcodename}-updates/${openstack_version}",
+      repos             => 'main',
+      include_src       => false,
+    }
   }
 }
