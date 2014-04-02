fdf6729bf3ea8e3a011525be14e9ea1cf85f54c9
Russell Sim <russell.sim@gmail.com>
2014-04-03 09:03:21 +1100

    Remove anchor replace with stage first
    Change-Id: Idbe1814eafc7adccfc0f44f0461c434a3ee8130e

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,11 @@
 class openstack {
 
-  anchor {'openstack::begin': }
-  anchor {'openstack::end': }
-
   # Set up repositories
-  class { 'openstack::repos': }
+  class { 'openstack::repos':
+      stage => first,
+  }
 
   # Packages
   class { 'openstack::packages': }
-
-  # Ensure that we set up the repositories before trying to install
-  # the packages
-  Anchor['openstack::begin']
-  -> Class['openstack::repos']
-  -> Class['openstack::packages']
-  -> Anchor['openstack::end']
 
 }
