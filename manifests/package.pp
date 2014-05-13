74f633bc0afc717779cd1542f4e3736a4963318d
Russell Sim <russell.sim@gmail.com>
2014-05-14 08:58:13 +1000

    Make compatible with puppetlabs mysql module
    Change-Id: Ide8a6d773e2db543b5100e8c80846b7810ce7938

--- manifests/package.pp
+++ manifests/package.pp
@@ -1,11 +1,16 @@
-class openstack::package {
+class openstack::package(
+  $mysql_module = '0.9'
+) {
 
   @package { 'python-keystone':
     ensure => present,
   }
 
-  @package { 'python-mysqldb':
-    ensure => present,
+  if ($mysql_module >= 2.2) {
+    require mysql::bindings
+    require mysql::bindings::python
+  } else {
+    include mysql::python
   }
 
   @package {'python-memcache':
