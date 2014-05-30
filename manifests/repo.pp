c0ede4bff52c4bd3e2882bada1633b77bb2262a4
Simon Kelada-Polupanov <simon.kelada@unimelb.edu.au>
2014-05-30 14:31:19 +1000

    Option to use proxy when adding apt key
    Change-Id: Idb200a4a8be1b2cdfd4d46bf086d48e0c3842ad7

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -4,9 +4,17 @@ class openstack::repo {
 
   if $lsbdistcodename == 'precise' {
 
+    if $::http_proxy and $::rfc1918_gateway == 'true' {
+      $key_options = "http-proxy=${::http_proxy}"
+    }
+    else {
+      $key_options = false
+    }
+
     apt::key { 'cloud-archive':
       key         => 'EC4926EA',
-      key_server => 'pgp.mit.edu',
+      key_server  => 'pgp.mit.edu',
+      key_options => $key_options,
     }
 
     apt::source { 'ubuntu-cloud-archive':
