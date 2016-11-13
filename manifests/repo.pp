8922095e6ea4280806760632560de172b9819e61
Sam Morrison <sorrison@gmail.com>
2016-11-14 09:12:02 +1100

    Improve logic on whether to push out the cloud archive
    Change-Id: Ie831f16798dd377de49edbf9dcc24ea2c251696f

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -1,33 +1,32 @@
+# Installs the opentack ubuntu cloud archive
 class openstack::repo {
 
   $openstack_version = hiera('openstack_version')
 
-  $supported_ubuntus = ['trusty', 'xenial']
-  $supported_openstacks = ['juno', 'kilo', 'liberty']
+  $supported = ['trusty-juno', 'trusty-kilo', 'trusty-liberty', 'trusty-mitaka',
+                'xenial-newton']
 
-  if $::lsbdistcodename in $supported_ubuntus {
-    if $openstack_version in $supported_openstacks {
+  if "${::lsbdistcodename}-${openstack_version}" in $supported {
 
-      if $::http_proxy and $::rfc1918_gateway == true {
-        $key_options = "http-proxy=${::http_proxy}"
-      }
-      else {
-        $key_options = false
-      }
+    if $::http_proxy and $::rfc1918_gateway == true {
+      $key_options = "http-proxy=${::http_proxy}"
+    }
+    else {
+      $key_options = false
+    }
 
-      apt::key { 'cloud-archive':
-        id      => '391A9AA2147192839E9DB0315EDB1B62EC4926EA',
-        server  => 'pgp.mit.edu',
-        options => $key_options,
-        notify  => Exec['apt_update'],
-      }
+    apt::key { 'cloud-archive':
+      id      => '391A9AA2147192839E9DB0315EDB1B62EC4926EA',
+      server  => 'pgp.mit.edu',
+      options => $key_options,
+      notify  => Exec['apt_update'],
+    }
 
-      apt::source { 'ubuntu-cloud-archive':
-        location    => 'http://mirrors.rc.nectar.org.au/ubuntu-cloud/ubuntu',
-        release     => "${::lsbdistcodename}-updates/${openstack_version}",
-        repos       => 'main',
-        include_src => false,
-      }
+    apt::source { 'ubuntu-cloud-archive':
+      location    => 'http://mirrors.rc.nectar.org.au/ubuntu-cloud/ubuntu',
+      release     => "${::lsbdistcodename}-updates/${openstack_version}",
+      repos       => 'main',
+      include_src => false,
     }
   }
 }
