a1f66d15a0591e308f8992a611e95fdf28155424
Sam Morrison <sorrison@gmail.com>
2018-11-13 14:19:35 +1100

    Pin kombu and amqp packages in pike
    Change-Id: I74f9c4c6c21e19ba93b1a87fe120bd09eac66530

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,8 @@ class openstack {
   # Set up repositories
   include ::openstack::repo
 
+  include ::openstack::oslo_messaging
+
   # Packages
   class { 'openstack::package':
     require => Class['openstack::repo'],
