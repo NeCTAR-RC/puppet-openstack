c0db5eb0cfa9e8738f384052a24295eb3a5757e9
Andy Botting <andy@andybotting.com>
2018-11-27 08:40:51 +1100

    Add bionic-rocky to supported list for repo
    Change-Id: Iadefe32ce58ad1e5e4699797f16198bac5b11b53
    Depends-On: https://review.rc.nectar.org.au/#/c/resplat-cloud/puppet-melbourne/+/15882/

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -4,7 +4,8 @@ class openstack::repo {
   $openstack_version = hiera('openstack_version')
 
   $supported = ['trusty-juno', 'trusty-kilo', 'trusty-liberty', 'trusty-mitaka',
-                'xenial-newton', 'xenial-ocata', 'xenial-pike', 'xenial-queens']
+                'xenial-newton', 'xenial-ocata', 'xenial-pike', 'xenial-queens',
+                'bionic-rocky']
 
   if "${::lsbdistcodename}-${openstack_version}" in $supported {
 
