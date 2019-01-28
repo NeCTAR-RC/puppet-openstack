44dca1e8fb0f12b229de2fffcb5cde9e5166ad6d
Sam Morrison <sorrison@gmail.com>
2019-01-29 09:29:35 +1100

    Remove pin on amqp/kombu, use upstream oslo.messaging
    Change-Id: I21e58d5331257145a617b01280d2cbe0ddb0ee43

--- manifests/oslo_messaging.pp
+++ manifests/oslo_messaging.pp
@@ -6,12 +6,19 @@ class openstack::oslo_messaging {
 
   if $openstack_version == 'pike' {
 
+    apt::pin { 'hold-oslo-messaging':
+      packages => 'python-oslo.messaging',
+      version  => '5.30.0-0ubuntu2~cloud0',
+      priority => 1001,
+    }
     apt::pin { 'hold-python-kombu':
+      ensure   => 'absent',
       packages => 'python-kombu',
       version  => '3.0.33-1ubuntu2',
       priority => 1001,
     }
     apt::pin { 'hold-python-amqp':
+      ensure   => 'absent',
       packages => 'python-amqp',
       version  => '1.4.9-1',
       priority => 1001,
