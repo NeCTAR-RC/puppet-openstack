cda133553fd995b1cfa74f4f63f8acc6cadfc6a1
Sam Morrison <sorrison@gmail.com>
2019-04-29 18:15:06 +1000

    Ensure oslo.messaging apt pins are removed
    These were needed temporarily in pike but have now been fixed
    in all openstack versions due to the newer python-amqp being
    released. Therefore these pins should be removed globally.
    
    Change-Id: I8db7171004306cfabee90715555257d4d3e4ead0

--- manifests/oslo_messaging.pp
+++ manifests/oslo_messaging.pp
@@ -2,26 +2,22 @@
 # See https://bugs.launchpad.net/oslo.messaging/+bug/1800957
 class openstack::oslo_messaging {
 
-  $openstack_version = hiera('openstack_version')
-
-  if $openstack_version == 'pike' {
-
-    apt::pin { 'hold-oslo-messaging':
-      packages => 'python-oslo.messaging',
-      version  => '5.30.0-0ubuntu2~cloud0',
-      priority => 1001,
-    }
-    apt::pin { 'hold-python-kombu':
-      ensure   => 'absent',
-      packages => 'python-kombu',
-      version  => '3.0.33-1ubuntu2',
-      priority => 1001,
-    }
-    apt::pin { 'hold-python-amqp':
-      ensure   => 'absent',
-      packages => 'python-amqp',
-      version  => '1.4.9-1',
-      priority => 1001,
-    }
+  apt::pin { 'hold-oslo-messaging':
+    ensure   => 'absent',
+    packages => 'python-oslo.messaging',
+    version  => '5.30.0-0ubuntu2~cloud0',
+    priority => 1001,
+  }
+  apt::pin { 'hold-python-kombu':
+    ensure   => 'absent',
+    packages => 'python-kombu',
+    version  => '3.0.33-1ubuntu2',
+    priority => 1001,
+  }
+  apt::pin { 'hold-python-amqp':
+    ensure   => 'absent',
+    packages => 'python-amqp',
+    version  => '1.4.9-1',
+    priority => 1001,
   }
 }
