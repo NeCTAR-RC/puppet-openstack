3b1f1cd28f0f9f9fa19977e7644b95f0e95d6921
Sam Morrison <sorrison@gmail.com>
2020-07-15 11:45:16 +1000

    Push cloud archive out to bionic-ussuri hosts
    Change-Id: I48e5c69f0254d2777d5dde819ee4bb10480e018a

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -7,7 +7,7 @@ class openstack::repo(
 
   $supported = ['trusty-juno', 'trusty-kilo', 'trusty-liberty', 'trusty-mitaka',
                 'xenial-newton', 'xenial-ocata', 'xenial-pike', 'xenial-queens',
-                'bionic-rocky', 'bionic-stein', 'bionic-train']
+                'bionic-rocky', 'bionic-stein', 'bionic-train', 'bionic-ussuri']
 
   if "${::lsbdistcodename}-${openstack_version}" in $supported {
 
