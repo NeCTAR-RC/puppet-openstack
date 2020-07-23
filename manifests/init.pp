a84e6bae021bd8cc9b0aecc76784427758c41116
Jake Yip <jake.yip@ardc.edu.au>
2020-07-23 10:04:04 +1000

    Remove oslo_messaging related package pins
    This is for Pike and no longer needed
    
    Change-Id: I39fd412d0615d01820b56ffa4010c190161ff3e5

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,8 +3,6 @@ class openstack {
   # Set up repositories
   include ::openstack::repo
 
-  include ::openstack::oslo_messaging
-
   # Packages
   class { 'openstack::package':
     require => Class['openstack::repo'],
