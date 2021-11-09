151ffeb5c66c4eb3eca5cd25d8490c6b78d1097b
Jacob Aharon <jacob.aharon@unimelb.edu.au>
2021-11-09 14:41:22 +1100

    Fail if the the ubuntu_version-openstack_version is unsupported
    Adds a native-supported list, so we will be able to handle all the known
    options.
    
    Change-Id: I308d02dd1f3604393cfa7090b26ea28b5152463b

--- manifests/repo/ubuntu.pp
+++ manifests/repo/ubuntu.pp
@@ -10,26 +10,35 @@ class openstack::repo::ubuntu(
                 'bionic-rocky', 'bionic-stein', 'bionic-train', 'bionic-ussuri',
                 'focal-victoria', 'focal-wallaby', 'focal-xena']
 
-  if "${::lsbdistcodename}-${openstack_version}" in $supported {
+  $native_supported = ['bionic-queens', 'focal-ussuri', 'jammy-yoga']
 
-    if defined('$::http_proxy') and str2bool($::rfc1918_gateway) {
-      $key_options = "http-proxy=${::http_proxy}"
-    }
-    else {
-      $key_options = undef
+
+  case "${::lsbdistcodename}-${openstack_version}" {
+
+    *$supported: {
+      if defined('$::http_proxy') and str2bool($::rfc1918_gateway) {
+        $key_options = "http-proxy=${::http_proxy}"
+      }
+      else {
+        $key_options = undef
+      }
+
+      apt::key { 'cloud-archive':
+        id      => '391A9AA2147192839E9DB0315EDB1B62EC4926EA',
+        server  => 'keyserver.ubuntu.com',
+        options => $key_options,
+        notify  => Exec['apt_update'],
+      }
+
+      apt::source { 'ubuntu-cloud-archive':
+        location => $mirror_url,
+        release  => "${::lsbdistcodename}-updates/${openstack_version}",
+        repos    => 'main',
+      }
     }
 
-    apt::key { 'cloud-archive':
-      id      => '391A9AA2147192839E9DB0315EDB1B62EC4926EA',
-      server  => 'keyserver.ubuntu.com',
-      options => $key_options,
-      notify  => Exec['apt_update'],
-    }
+    *$native_supported: {}
 
-    apt::source { 'ubuntu-cloud-archive':
-      location => $mirror_url,
-      release  => "${::lsbdistcodename}-updates/${openstack_version}",
-      repos    => 'main',
-    }
+    default: {fail("${::lsbdistcodename}-${openstack_version} is not supported")}
   }
 }
