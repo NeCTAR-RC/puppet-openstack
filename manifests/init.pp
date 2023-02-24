4a27ec5ae37592ee8a85bb498fe2d91e603b2886
Sam Morrison <sorrison@gmail.com>
2023-02-24 11:14:22 +1100

    Remove empty packages class
    Change-Id: I553602e14c878756b1c9ddf41cba2c0c10c38870

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,6 @@
 class openstack {
 
   # Set up repositories
-  include ::openstack::repo
-
-  # Packages
-  class { 'openstack::package':
-    require => Class['openstack::repo'],
-  }
+  include openstack::repo
 
 }
--- manifests/package.pp
+++
@@ -1,3 +0,0 @@
-class openstack::package {
-
-}
